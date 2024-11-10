ab94d734e15d3d6344d07c4f5ce9a20389fe88ac
vishwas-k-gowda <vishwas2846@gmail.com>
2024-11-10 14:29:03 +0530

    hallow pattern

---
+++ hallow_pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int row,col;
+    cin>>row>>col;
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=col;j++){
+            if(j==1||i==1||i==row||j==col){
+                cout<<"*";
+            }
+        else{
+             cout<<" ";
+        }
+           
+            }
+            cout<<endl;  
+    }
+}
