8e21b0b77c15feb9140cd73aeb678622cfa61429
vishwas-k-gowda <vishwas2846@gmail.com>
2024-11-11 21:02:02 +0530

    nbr_pyramid

--- number_pyramid.cpp
+++ number_pyramid.cpp
@@ -5,8 +5,7 @@ int main(){
     cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
-            int x=i;
-            cout<<x;
+            cout<<i<<" ";
         }
         cout<<endl;
     }
