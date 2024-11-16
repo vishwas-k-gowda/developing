c502cc16fa1ccdf6d4404be2b04700ab91ce0b13
vishwas-k-gowda <vishwas2846@gmail.com>
2024-11-16 22:50:56 +0530

    vowel

---
+++ vowel.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char alphabet;
+    cout<<"enter alphabet\n";
+    cin>>alphabet;
+    switch(alphabet){
+      case 'a':
+      cout<<"it is vowel";
+      break;
+        case 'e':
+      cout<<"it is vowel";
+      break;
+        case 'i':
+      cout<<"it is vowel";
+      break;
+        case 'o':
+      cout<<"it is vowel";
+      break;
+        case 'u':
+      cout<<"it is vowel";
+      break;
+      default:
+      cout<<"type another operator";
+      break;
+    }
+}
