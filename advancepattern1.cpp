e71124e0a56a228151be0e961ada69249a93f8c1
vishwas-k-gowda <vishwas2846@gmail.com>
2024-11-19 21:25:38 +0530

    advancepattern1

---
+++ advancepattern1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int x=0;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=x+i;j++){
+            cout<<j;
+        } 
+        x++;
+        cout<<endl;
+    }
+}
